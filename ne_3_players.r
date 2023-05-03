1abb372456db136cb1021e7132aaabe643aeced4
mzheng21 <mzheng21@stanford.edu>
2023-05-03 16:47:40 -0700

    starting script for 3 players

---
+++ ne_3_players.r
@@ -0,0 +1,9 @@
+# Calculate total ability and rank of 3 players for every possible collaboration
+
+# Load libraries
+library(tidyverse)
+
+# Generate individual ability
+n_1 <- 1/3
+n_2 <- 2/3
+n_3 <- 1
